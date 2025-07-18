9c75b30538aa645c9e68121493439fb75816bf75
Muhammad Umer <109547152+UmerGulzar223@users.noreply.github.com>
2025-07-18 23:51:36 +0500

    .

--- app.py
+++ app.py
@@ -14,5 +14,17 @@ def about():
 def projects():
     return render_template("projects.html")
 
+@app.route('/skills')
+def skills():
+    return render_template("skills.html")
+
+@app.route('/experience')
+def experience():
+    return render_template("experience.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

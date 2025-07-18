65e5b9c58bf438f0bcdb1ffd18d65d57ce75d07a
Muhammad Umer <109547152+UmerGulzar223@users.noreply.github.com>
2025-07-18 23:30:34 +0500

    adding complete porfolio file

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, CI/CD!"
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ def test_home():
     from app import app
     client = app.test_client()
     response = client.get('/')
-    assert response.data == b'Hello, CI/CD!'
+    assert response.status_code == 200
